31881a945c81e9d4eb9c809cb9c01553868fe988
Niranga Lakshan <nirangawh@gmail.com>
2025-02-08 16:39:41 +0530

    Configured the Swagger and Class validators

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import rateLimit from 'express-rate-limit';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
@@ -9,7 +13,8 @@ async function bootstrap(): Promise<void> {
   // Set global api end point
   app.setGlobalPrefix(`${process.env.APP_VERSION}`);
 
-  await app.listen(process.env.APP_PORT ?? 3000);
+  // Set global validation pipe
+  app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors();
 
@@ -35,5 +40,19 @@ async function bootstrap(): Promise<void> {
       max: 500, // limit each IP to 500 requests per windowMs
     }),
   );
+
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle('TO-DO APIs')
+    .setDescription('API documentation for TO-DO-API')
+    .setVersion(`1.0.0`)
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(`${process.env.APP_VERSION}/api/docs`, app, document);
+
+  //Setting up port
+  await app.listen(process.env.APP_PORT ?? 3000);
 }
 bootstrap();
