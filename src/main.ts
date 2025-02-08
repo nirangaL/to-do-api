dc51af875fa3c91c49ff8784e3d7b9a28562bc85
Niranga Lakshan <nirangawh@gmail.com>
2025-02-08 14:44:30 +0530

    Secured app setting up Helmet and Rate limiting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  // Security Headers with Helmet
+  app.use(helmet());
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+      },
+    }),
+  );
+  app.use(
+    helmet.permittedCrossDomainPolicies({
+      permittedPolicies: 'none',
+    }),
+  );
+
+  // Rate Limiting
+  app.use(
+    rateLimit({
+      windowMs: 2 * 60 * 1000, // 2 minutes
+      max: 500, // limit each IP to 500 requests per windowMs
+    }),
+  );
 }
 bootstrap();
